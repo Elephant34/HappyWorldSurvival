3795e482e6ef9e1a94e6ccd479745acdddaad1ae
Elephant34 <51174423+Elephant34@users.noreply.github.com>
2020-05-16 07:22:21 +0100

    Added a main menu GUI actions still WIP

--- HappyWorldSurvival.py
+++ HappyWorldSurvival.py
@@ -1,7 +1,11 @@
 '''
 Starts the game opening to the main menu
 '''
+import pathlib
+
 import arcade
+
+from assets.buttons import MenuButton
 
 
 class GameWindow(arcade.Window):
@@ -16,10 +20,69 @@ class GameWindow(arcade.Window):
         '''
         super().__init__()
 
-    def setup(self):
+        self.on_menu = True
+
+    def main_menu(self):
         '''
-        Starts the game going
+        loads the main menu GUI
         '''
+
+        self.menu_theme = arcade.gui.Theme()
+
+        normal = pathlib.Path("static/buttons/Normal.png")
+        hover = pathlib.Path("static/buttons/Hover.png")
+        clicked = pathlib.Path("static/buttons/Clicked.png")
+        locked = pathlib.Path("static/buttons/Locked.png")
+
+        self.menu_theme.add_button_textures(normal, hover, clicked, locked)
+
+        self.button_list.append(
+            MenuButton(self, 350, "Load Game", self.load_game)
+        )
+        self.button_list.append(
+            MenuButton(self, 250, "Connect to Game", self.connect_server)
+        )
+        self.button_list.append(
+            MenuButton(self, 150, "Quit", self.quit_game)
+        )
+
+    def on_draw(self):
+        '''
+        Draws the game to the screen
+        '''
+        arcade.start_render()
+
+        if self.on_menu:
+            for button in self.button_list:
+                if button.active:
+                    button.draw()
+            arcade.draw_text(
+                "Happy World Survival",
+                self.get_size()[0] / 2,
+                500,
+                arcade.color.WHITE,
+                60,
+                anchor_x="center",
+                anchor_y="center"
+            )
+
+    def load_game(self):
+        '''
+        Loads the game by starting a server and then connecting
+        '''
+        print("Load Game")
+
+    def connect_server(self):
+        '''
+        Gets an ip from user and attempts to connect
+        '''
+        print("Connect Server")
+
+    def quit_game(self):
+        '''
+        Exits the game
+        '''
+        self.close()
 
 
 if __name__ == "__main__":
@@ -28,6 +91,6 @@ if __name__ == "__main__":
     game.set_caption("Happy World Survival")
     game.set_size(800, 600)
 
-    game.setup()
+    game.main_menu()
 
     arcade.run()
